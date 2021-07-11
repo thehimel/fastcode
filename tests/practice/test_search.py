7cfc1087e18511b869842dbed7b2d3fef9f92bc2
Himel Das <himelhub@gmail.com>
2021-07-12 00:58:26 +0200

    configured for windows

--- tests/practice/test_search.py
+++ tests/practice/test_search.py
@@ -1,13 +1,6 @@
 import pytest
 from src.practice.search.binary import binary_search
 
-import sys
-import os
-
-myPath = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, myPath + "/../")
-
-
 def test_binary():
     array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     target = 6
